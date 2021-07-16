a25c8f333de4e4114c071f289bf52d96ce3250ee
tycjan <tycjano@o2.pl>
2021-07-16 08:59:12 +0200

    Update pleng_gap.sh

--- pleng_gap.sh
+++ pleng_gap.sh
@@ -2,20 +2,24 @@
 
 set -e
 
-dir="/pleng/gap2"
+dir="/pleng/gap"
+archive="Archive"
 find_cmd="/usr/bin/find"
 gzip_cmd="/usr/bin/gzip"
 rm_cmd="/usr/bin/rm -rf"
+mk_cmd="/usr/bin/mkdir -p"
+mv_cmd="/usr/bin/mv"
+du_cmd="/usr/bin/du -sh"
 
 usage(){
 
     echo "Usage: $0 [--restore|-r] {from date} {to date} | [--clean|-c] {c} {r}"
     echo ""
     echo "where:"
-    echo "     --restore - accepts given date format as <YYYY-MM-DD> only."
+    echo "     --restore - requires two date args in given format <YYYY-MM-DD> <YYYY-MM-DD>"
     echo "     --clean   - accepts optional parameters: "
-    echo "                 * {c} - compress files older than (default 7),"
-    echo "                 * {r} - remove files older than (default 360)."
+    echo "                 * {c} - compress files older than (default 7) days,"
+    echo "                 * {r} - remove files older than (default 370) days."
     echo ""
 
         exit 1
@@ -54,29 +58,37 @@ restore_files(){
 
     local from=${1}
     local to=${2}
-    local mtime="00:00:01"
-    local ntime="23:59:59"
+    local range=${from}
 
     [[ "${from}" && "${to}" ]] || {
 
-        tPrint "Restore parameter requires date range in given format <YYYY-MM-DD> <YYYY-MM-DD>." "DEBUG"
+        tPrint "Restore parameter requires two dates in given format <YYYY-MM-DD> <YYYY-MM-DD>"
         exit 1
 
     }
 
     check_date "${from}" && check_date "${to}"
 
-    mapfile -t to_unzip < <($find_cmd "${dir}" -type f -name "*.gz" -newermt "${from} ${mtime}" ! -newermt "${to} ${ntime}" -print)
+    tPrint "Looking for files to restore between ${from} and ${to} (...)"
+
+    while [[ "${range}" < "${to}" || "${range}" == "${to}" ]]
+    do
+        mapfile -t -O "${#to_unzip[@]}" to_unzip < <($find_cmd "${dir}" -type f -name "*${range//-/}.gz" -print)
+        range=$(date -I -d "$range + 1 day")
+    done
 
     (( "${#to_unzip[@]}" > 0 )) && {
 
-	tPrint "I've found ${#to_unzip[@]} file(s) to restore. Processing..."
-	for f in "${to_unzip[@]}"
-	do
-	    tPrint " - Restoring file ${f##*/}"
-	    $gzip_cmd -d "${f}"
-	done
+        tPrint "I've found ${#to_unzip[@]} file(s) to restore. Processing..."
+        for f in "${to_unzip[@]}"
+        do
+            tPrint " - Restoring file ${f##*/}"
+            ${gzip_cmd} -d "${f}" && ${mv_cmd} "${f%.*}" "${dir}/"
+        done
 
+    tPrint "--------------------------------------------------------"
+    tPrint "Restore completed with status OK. Restored ${#to_unzip[@]} file(s)."
+    tPrint "--------------------------------------------------------"
     exit 0
 
     }
@@ -88,7 +100,14 @@ restore_files(){
 clean_files(){
 
     local fcompres=${1:-7}
-    local fdelete=${2:-360}
+    local fdelete=${2:-370}
+
+    (( "${fdelete}" > "${fcompres}" )) || {
+        tPrint "Parameter {c} cannot be greater or equal to {r}" "DEBUG"
+        exit 1
+    }
+
+    (( "${fcompres}" == 1 )) && fcompres="-1" # Start compression from today.
 
     mapfile -t to_archive < <($find_cmd "${dir}" -type f -name "*BUZ0P*" -not -name "*.gz" -mtime +"${fcompres}" -print)
 
@@ -97,8 +116,8 @@ clean_files(){
         tPrint "I've found ${#to_archive[@]} file(s) to archive. Processing..."
         for f in "${to_archive[@]}"
         do
-	    tPrint " - Archiving file ${f##*/}"
-            $gzip_cmd "${f}"
+            tPrint " - Archiving file ${f##*/}"
+            ${gzip_cmd} "${f}" && ${mv_cmd} "${f}.gz" "${dir}/${archive}/"
         done
 
     } || tPrint "Nothing found to archive."
@@ -107,24 +126,42 @@ clean_files(){
 
     (( "${#to_remove[@]}" > 0 )) && {
 
-	tPrint "I've found ${#to_remove[@]} file(s) to remove. Processing..."
+        tPrint "I've found ${#to_remove[@]} file(s) to remove. Processing..."
         for f in "${to_remove[@]}"
         do
-    	    tPrint " - Removing file ${f##*/}"
-            $rm_cmd "${f}"
+            tPrint " - Removing file ${f##*/}"
+            ${rm_cmd} "${f}"
         done
 
 
     } || tPrint "Nothing found to remove."
 
+
+    tPrint "Cleanup completed with status OK. Archieved: ${#to_archive[@]} file(s). Removed: ${#to_remove[@]} file(s)."
+
     exit 0
 
 }
 
+# Main
+
 [[ -d "${dir}" ]] || {
 
-    tPrint "Folder ${dir} does not exist or user $(whoami) cannot access it. Leaving the script!" "FATAL"
+    tPrint "Folder ${dir} does not exist. Leaving the script!" "FATAL"
     exit 1
+}
+
+[[ -w "${dir}" ]] || {
+
+    tPrint "Folder ${dir} is not writable by user $(whoami). Leaving the script!" "FATAL"
+    exit 1
+}
+
+[[ -d "${dir}/${archive}" ]] || {
+
+    ${mk_cmd} "${dir}/${archive}"
+    (( "${?}" == "0" )) && tPrint "Folder ${archive} sucessfully created."
+
 }
 
 [[ ${1} == "--restore" || ${1} == "-r" ]] && {
@@ -133,6 +170,8 @@ clean_files(){
         to="${3}"
         shift
 
+        tPrint "Starting script in RESTORE mode."
+        tPrint "Current usage of ${dir}: $(${du_cmd} ${dir} |cut -f1)"
         restore_files "${1}" "${2}"
 
 }
@@ -142,6 +181,9 @@ clean_files(){
         fcompres="${2}"
         fdelete="${3}"
         shift
+
+        tPrint "Starting script in CLEAN mode."
+        tPrint "Current usage of ${dir}: $(${du_cmd} ${dir} |cut -f1)"
 
         clean_files "${1}" "${2}"
 
